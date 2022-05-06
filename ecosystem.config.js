dd41f71a3e06c333353c4b388d9e18ca6fad14e4
hisakiyo <hisakiyo.games@gmail.com>
2022-05-06 16:38:45 +0000

    Set PM2 to fork

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'NASA-Agregator',
-      exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
+      exec_mode: 'fork',
+      //instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
