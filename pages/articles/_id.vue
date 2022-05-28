469b7ea64bf099d072cd11c52697465485314e11
Hisakiyo <34187647+hisakiyo@users.noreply.github.com>
2022-05-28 23:30:22 +0200

    Update _id.vue

--- pages/articles/_id.vue
+++ pages/articles/_id.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="py-20 w-full">
     <section class="relative text-center px-10 mt-20">
-      <router-link to="/news/" class="absolute font-bold float-left -mt-16">&lt; {{ $t("back") }}</router-link>
+      <router-link :to="localePath('/news/')" class="absolute font-bold float-left -mt-16">&lt; {{ $t("back") }}</router-link>
       <h1 class="text-2xl tracking-tight font-extrabold sm:text-4xl text-white">{{ article.title }}</h1>
       <p class="mt-2 max-w-3xl mx-auto text-lg">{{ $t("published_date") }} {{ new Date(article.date).toLocaleDateString() }}</p>
     </section>
