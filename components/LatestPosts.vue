e22c22d85e019d26e5341f45e514ce7aa75bfc5a
Hisakiyo <34187647+hisakiyo@users.noreply.github.com>
2022-05-28 23:27:53 +0200

    Update LatestPosts.vue

--- components/LatestPosts.vue
+++ components/LatestPosts.vue
@@ -7,7 +7,7 @@
 
     <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 lg:gap-4 px-10">
       <li v-for="article in articles" :key="article.id">
-        <nuxt-link :to="`/articles/${article.id}/`" class="w-full h-full mt-5 mb-2 lg:mb-8 px-6 py-4 bg-slate-800 shadow-lg overflow-hidden ring-1 ring-white/10 hover:ring-white/20 cursor-pointer ring-inset">
+        <nuxt-link :to="localePath(`/articles/${article.id}/`)" class="w-full h-full mt-5 mb-2 lg:mb-8 px-6 py-4 bg-slate-800 shadow-lg overflow-hidden ring-1 ring-white/10 hover:ring-white/20 cursor-pointer ring-inset">
           <h2 class="text-lg font-medium">{{ article.title }}</h2>
           <p class="text-sm text-slate-400">{{ $t("published_date") }} {{ new Date(article.date).toLocaleDateString() }}</p>
           <p class="text-sm text-slate-200 mt-2">{{article.description.substring(0, 300) }}</p>
