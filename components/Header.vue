4a1082376e71fb62e5ca409ccc06d51497a155fb
Sotramp <sotraamp@gmail.com>
2022-05-28 18:10:20 +0200

    Fix logo static link + menu links

--- components/Header.vue
+++ components/Header.vue
@@ -3,13 +3,13 @@
         <div class="header-inner flex text-white m-auto py-4 justify-between">
           <div class="flex items-center">
             <router-link to="/" class="logo-container">
-              <img id="logo" src="nasa_agregator_logo_min.png" alt="NASA Agregator">
+              <img id="logo" src="/nasa_agregator_logo_min.png" alt="NASA Agregator">
             </router-link>
             <nav>
               <router-link to="/" class="uppercase font-semibold">{{ $t("home") }}</router-link>
-              <router-link to="/news" class="uppercase font-semibold">{{ $t("news") }}</router-link>
-              <router-link to="/iss" class="uppercase font-semibold">ISS Explorer</router-link>
-              <router-link to="/about" class="uppercase font-semibold">{{ $t("about") }}</router-link>
+              <router-link to="/news/" class="uppercase font-semibold">{{ $t("news") }}</router-link>
+              <router-link to="/iss/" class="uppercase font-semibold">ISS Explorer</router-link>
+              <router-link to="/about/" class="uppercase font-semibold">{{ $t("about") }}</router-link>
             </nav>
           </div>
           <div class="switch-languages">
