cc17c0ef59a45fd222f8116d71cfb33c5f2340b6
Sotramp <sotraamp@gmail.com>
2022-05-28 21:07:31 +0200

    Front about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,37 +1,10 @@
 <template>
-  <div>
-    <p>{{ $t("welcome") }}</p>
-    <v-btn @click="test()">Test</v-btn>
-    <p>{{ testContent }}</p>
+  <div class="w-[900px] m-auto pt-32 pb-32">
+    <h1 class="text-4xl tracking-tight font-extrabold sm:text-5xl text-white pb-6">À propos</h1>
+    <div class="mt-2 max-w-3xl mx-auto text-lg">
+      <p>Ce site internet a été pensé et développé par MARETTE Gauthier et CLAIRET Damien, dans le cadre d'un projet sur les Web Services durant leur première année de Master I2L.</p>
+      <p class="pt-8">Il exploite les API publiques de la NASA ainsi que des API internes permettant de publier des articles ainsi que de gérer les commentaires postés sur le site (système de validation).</p>
+      <p class="pt-8">Le projet dispose d'une interface d'administration pour la gestion des articles et des commentaires, accessible en <a class="border-b pb-0.5" :href="/admin/">cliquant ici.</a></p>
+    </div>
   </div>
-
 </template>
-<script>
-import axios from 'axios'
-
-export default {
-  data() {
-    return {
-      testContent: '',
-    }
-  },
-  head() {
-    return {
-      title: 'A propos',
-    }
-  },
-  methods: {
-    async test() {
-      await axios
-          .get('/api/news')
-          .then((getResponse) => {
-            this.testContent = getResponse.data
-          })
-          .catch((e) => {
-            // eslint-disable-next-line no-console
-            console.log(e)
-          })
-    }
-  },
-}
-</script>
